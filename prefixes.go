e084ba4704c6fbd75b5593aae6ada93157c7e67d
vladimirb <nordborn@gmail.com>
2018-12-31 10:15:16 +0300

    upd prefixes

--- prefixes.go
+++ prefixes.go
@@ -4,11 +4,12 @@ package golog
 // It is possible to redefine them,
 // but most probably, you don't need to do it.
 var (
-	PrefixTrace    = "[TRACE] "
-	PrefixDebug    = "[DEBUG] "
-	PrefixInfo     = "[INFO] "
-	PrefixWarning  = "[WARNING] "
-	PrefixError    = "[ERROR] "
-	PrefixCritical = "[CRITICAL] "
-	PrefixFatal    = "[FATAL] "
+	PrefixTrace    = "[TRC] "
+	PrefixDebug    = "[DBG] "
+	PrefixInfo     = "[INF] "
+	PrefixWarning  = "[WRN] "
+	PrefixError    = "[ERR] "
+	PrefixCritical = "[CRT] "
+	PrefixPanic    = "[PNC] "
+	PrefixFatal    = "[FTL] "
 )
